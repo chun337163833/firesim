65c4283a405895066224b40e23d5aa235ce20390
1001zippy <1001zippy@gmail.com>
2013-10-13 12:45:47 -0400

    set up canvas

--- firesim/firesim.js
+++ firesim/firesim.js
@@ -1,75 +1,30 @@
-//set main namespace
+// set main namespace 
 goog.provide('firesim');
 
-
-//get requirements
+// get requirements 
 goog.require('lime.Director');
 goog.require('lime.Scene');
 goog.require('lime.Layer');
-goog.require('lime.Circle');
-goog.require('lime.Label');
-goog.require('lime.animation.Spawn');
-goog.require('lime.animation.FadeTo');
-goog.require('lime.animation.ScaleTo');
-goog.require('lime.animation.MoveTo');
 
+// entrypoint 
+firesim.start = function(){     
 
-// entrypoint
-firesim.start = function(){
+    //game object
+    var gameObj = {
+        width: 320,
+        height: 480,
+    }
 
-	var director = new lime.Director(document.body,1024,768),
-	    scene = new lime.Scene(),
+    var director = new lime.Director(document.body,gameObj.width,gameObj.height);     
+    director.makeMobileWebAppCapable();
+    director.setDisplayFPS(true); // set fps to show in corner
 
-	    target = new lime.Layer().setPosition(512,384),
-        circle = new lime.Circle().setSize(150,150).setFill(255,150,0),
-        lbl = new lime.Label().setSize(160,50).setFontSize(30).setText('TOUCH ME!'),
-        title = new lime.Label().setSize(800,70).setFontSize(60).setText('Now move me around!')
-            .setOpacity(0).setPosition(512,80).setFontColor('#999').setFill(200,100,0,.1);
+    var gameScene = new lime.Scene().setRenderer(lime.Renderer.CANVAS); // render canvas
+    var landLayer = new lime.Layer().setAnchorPoint(0, 0);
+    var controlsLayer = new lime.Layer().setAnchorPoint(0, 0);
 
+    gameScene.appendChild(landLayer);
+    gameScene.appendChild(controlsLayer);
 
-    //add circle and label to target object
-    target.appendChild(circle);
-    target.appendChild(lbl);
-
-    //add target and title to the scene
-    scene.appendChild(target);
-    scene.appendChild(title);
-
-	director.makeMobileWebAppCapable();
-
-    //add some interaction
-    goog.events.listen(target,['mousedown','touchstart'],function(e){
-
-        //animate
-        target.runAction(new lime.animation.Spawn(
-            new lime.animation.FadeTo(.5).setDuration(.2),
-            new lime.animation.ScaleTo(1.5).setDuration(.8)
-        ));
-
-        title.runAction(new lime.animation.FadeTo(1));
-
-        //let target follow the mouse/finger
-        e.startDrag();
-
-        //listen for end event
-        e.swallow(['mouseup','touchend'],function(){
-            target.runAction(new lime.animation.Spawn(
-                new lime.animation.FadeTo(1),
-                new lime.animation.ScaleTo(1),
-                new lime.animation.MoveTo(512,384)
-            ));
-
-            title.runAction(new lime.animation.FadeTo(0));
-        });
-
-
-    });
-
-	// set current scene active
-	director.replaceScene(scene);
-
+    director.replaceScene(gameScene); 
 }
-
-
-//this is required for outside access after code is compiled in ADVANCED_COMPILATIONS mode
-goog.exportSymbol('firesim.start', firesim.start);
